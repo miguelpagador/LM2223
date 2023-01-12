26ca93046d0211abc6bcf90ebdd4425e3ac277cd
Miguel <miguelpagador04@iesamachado.org>
2023-01-12 17:57:58 +0100

    javascript

---
+++ Javascript/pdf1/circulos.js
@@ -0,0 +1,18 @@
+function norojo() {
+    document.getElementById('rojo').style.display='none'
+}
+function rojo() {
+    document.getElementById('rojo').style.display='block'
+}
+function noazul() {
+    document.getElementById('azul').style.display='none'
+}
+function azul() {
+    document.getElementById('azul').style.display='block'
+}
+function norosa() {
+    document.getElementById('rosa').style.display='none'
+}
+function rosa() {
+    document.getElementById('rosa').style.display='block'
+}
