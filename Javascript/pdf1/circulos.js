cb707848bdf9e9c4ef4542650cb6a9065375e4a9
miguelpagador <miguelpagador04@iesamachado.org>
2023-01-13 14:21:44 +0100

    añadido

---
+++ Javascript/pdf1/circulos.js
@@ -0,0 +1,9 @@
+function ocultar1(){
+    document.getElementById('circulo1').style.display= 'none';
+}
+function ocultar2(){
+    document.getElementById('circulo2').style.display= 'none';
+}
+function ocultar3(){
+    document.getElementById('circulo3').style.display= 'none';
+}
