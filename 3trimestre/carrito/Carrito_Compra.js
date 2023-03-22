111fdc0509b8eb40653b0d28feb4f90ad911a786
miguelpagador <miguelpagador04@iesamachado.org>
2023-03-22 10:39:22 +0100

    añadido

--- 3trimestre/carrito/Carrito_Compra.js
+++ 3trimestre/carrito/Carrito_Compra.js
@@ -1,22 +1,63 @@
 
-function ordenadores(){
+function ordenadores() {
     var pc1 
     var pc2
     var pc3
     var pc4
     pc1 = "<img src='Imagenes/Torre1_pc.png'>";
-    document.getElementById("pc1").innerHTML=pc1
+    document.getElementById("pc_a").innerHTML=pc1
     pc2 = "<img src='Imagenes/Torre2_pc.png'>";
-    document.getElementById("pc2").innerHTML=pc2
+    document.getElementById("pc_b").innerHTML=pc2
     pc3 = "<img src='Imagenes/Torre3_pc.png'>";
-    document.getElementById("pc3").innerHTML=pc3
+    document.getElementById("pc_c").innerHTML=pc3
     pc4 = "<img src='Imagenes/Torre4_pc.png'>";
-    document.getElementById("pc4").innerHTML=pc4;
+    document.getElementById("pc_d").innerHTML=pc4;
 }
 
-var teclados = document.getElementById("teclados");
-teclados.addEventListener("click", function(){
-    var img2 
-    img2 = "<img src='Imagenes/teclado.png'>";
-    document.getElementById("teclado1").innerHTML=img2;
-})
+function teclados(){
+    var tcl1;
+    var tcl2;
+    var tcl3;
+    var tcl4;
+    tcl1 = "<img src='Imagenes/teclado1.png'>";
+    document.getElementById("teclado_a").innerHTML=tcl1;
+    tcl2 = "<img src='Imagenes/teclado2.png'>";
+    document.getElementById("teclado_b").innerHTML=tcl2;
+    tcl3 = "<img src='Imagenes/teclado3.png'>";
+    document.getElementById("teclado_c").innerHTML=tcl3;
+    tcl4 = "<img src='Imagenes/teclado4.png'>";
+    document.getElementById("teclado_d").innerHTML=tcl4;
+}
+
+function ratones(){
+    var rt1;
+    var rt2;
+    var rt3;
+    var rt4;
+    rt1 = "<img src='Imagenes/raton1.png'>";
+    document.getElementById("ratones_a").innerHTML=rt1;
+    rt2 = "<img src='Imagenes/raton2.png'>";
+    document.getElementById("ratones_b").innerHTML=rt2;
+    rt3 = "<img src='Imagenes/raton3.png'>";
+    document.getElementById("ratones_c").innerHTML=rt3;
+    rt4 = "<img src='Imagenes/raton4.png'>";
+    document.getElementById("ratones_d").innerHTML=rt4;
+}
+
+
+function altavoces(){
+    var alt1;
+    var alt2;
+    var alt3;
+    var alt4;
+    alt1 = "<img src='Imagenes/altavoz1.png'>";
+    document.getElementById("altavoces_a").innerHTML=alt1;
+    alt2 = "<img src='Imagenes/altavoz2.png'>";
+    document.getElementById("altavoces_b").innerHTML=alt2;
+    alt3 = "<img src='Imagenes/altavoz3.png'>";
+    document.getElementById("altavoces_c").innerHTML=alt3;
+    alt4 = "<img src='Imagenes/altavoz4.png'>";
+    document.getElementById("altavoces_d").innerHTML=alt4;
+}
+
+
