dd53c6cf93da285337dcd311686842264fa84387
Miguel <miguelpagador04@iesamachado.org>
2023-04-26 11:19:35 +0200

    añadido

---
+++ 3trimestre/jquery/jQuery1/ej2.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $("#btn-ocultar").click(function (e) { 
+        e.preventDefault();
+        $("#encabezado").hide();
+        $(".pares").hide();
+    });
+    $("#btn-mostrar").click(function (e) { 
+        e.preventDefault();
+        $("#encabezado").show();
+        $(".pares").show();
+    });
+});
